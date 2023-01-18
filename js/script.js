7298499162b216202af0c8ae849c0e864b91de3d
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2023-01-18 08:57:12 -0500

    different api

--- js/script.js
+++ js/script.js
@@ -20,16 +20,30 @@ if (navigator.serviceWorker) {
 */
 // code from: https://www.youtube.com/watch?v=670f71LTWpM
 
-var request = new XMLHttpRequest();
-
-request.open('GET', 'http://cakes.ckl.io/cakes/');
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+    '<img src="' + 
+      jsonData.url + 
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
+  } else {
+    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
   }
-};
+  } catch (err) {
+    console.log(err)
+  }
+}
 
-request.send();
+getImage("https://pokeapi.co/api/v2/")
